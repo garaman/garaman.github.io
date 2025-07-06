30503cd2c8c8c33035559f1172e1f40041a88fa7
Hyun Soo An <ahs20103@gmail.com>
2025-07-06 17:18:57 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,22 +7,12 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React123
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />        
         <br />
         <Link to="/gugudan" className="App-link">
           구구단 페이지로 이동
         </Link>
+        
       </header>
     </div>
   );
