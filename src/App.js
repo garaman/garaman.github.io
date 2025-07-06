ba1c183c5f1047846ccfeaabcb08f175bc5de7a7
Hyun Soo An <ahs20103@gmail.com>
2025-07-06 17:32:50 +0900

    20250706_Layout

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import './App.css';
 import { Link, Routes, Route } from 'react-router-dom';
 import Gugudan from './components/Gugudan';
 
+// Home 컴포넌트: App-logo만 중앙정렬로 표시
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />        
-        <br />
-        <Link to="/gugudan" className="App-link">
-          구구단 페이지로 이동
-        </Link>
-        
-      </header>
+    <div className="home-content">
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/gugudan" element={<Gugudan />} />
-    </Routes>
+    <div className="app-layout">
+      <nav className="app-menu">
+        <ul>
+          <li>
+            <Link to="/" className="App-link">Home</Link>
+          </li>
+          <li>
+            <Link to="/gugudan" className="App-link">구구단</Link>
+          </li>
+        </ul>
+      </nav>
+      <main className="app-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gugudan" element={<Gugudan />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
