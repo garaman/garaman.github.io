29f0a6372083a4ca98917cb8dc2f8edc71f1e564
Hyun Soo An <ahs20103@gmail.com>
2025-07-06 17:17:30 +0900

    gugudan css

--- src/components/gugudan.js
+++ src/components/gugudan.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Gugudan.css';
 
 class Gugudan extends Component {
   state = {
@@ -37,18 +38,25 @@ class Gugudan extends Component {
 
   render() {
     return (
-      <div>
-        <div>{this.state.first} 곱하기 {this.state.second}는?</div>
+      <div className="gugudan-container">
+        <div className="gugudan-question">
+          {this.state.first} 곱하기 {this.state.second}는?
+        </div>
         <form onSubmit={this.onSubmit}>
           <input
             ref={this.input}
             type="number"
             value={this.state.value}
             onChange={this.onChange}
+            className="gugudan-input"
+            placeholder="정답 입력"
+            autoFocus
           />
-          <button>입력!</button>
+          <button className="gugudan-button">입력!</button>
         </form>
-        <div>{this.state.result}</div>
+        <div className="gugudan-result">
+          {this.state.result ? this.state.result : '결과가 여기에 표시됩니다.'}
+        </div>
       </div>
     );
   }
